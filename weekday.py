52fba3c92805c0b0042a9969f41faf891aeff22f
Alexander Lübeck <ndsvw@bitbucket.org>
2017-08-23 22:22:25 +0200

    BUGFIX: Schaltjahrkorrektur und Monatsnamen hinzugefügt

--- weekday.py
+++ weekday.py
@@ -1,17 +1,24 @@
 import sys
 
 a = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]
+b = ["Januar", "Februar", "Maerz", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"]
 
 def getWochentag(tag, monat, jahr):
-    if jahr < 1900 || jahr > 2099:
-        return "Es können nur Wochentage zwischen 1900 und 2099 berechnet werden."
-    
+    if (jahr < 1900)or(jahr > 2099):
+        return "Es koennen nur Wochentage zwischen 1900 und 2099 berechnet werden."
+
     sum = tag + getMonatsCode(monat, jahr) + getJahresCode(jahr)
     print("Tag = " + str(tag))
     print("Monat = " + str(getMonatsCode(monat, jahr)))
     print("Jahr = " + str(getJahresCode(jahr)))
     print("Summe = " + str(sum))
     print("Summe % 7 = " + str(sum % 7))
+
+    if isSchaltjahr(jahr) and monat < 3:
+        print("1 abziehen, da es ein " + b[monat-1] + " im Schaltjahr ist")
+        print("Summe = ", str(sum), "- 1 = " + str(sum-1))
+        sum = sum - 1
+
     wochentagCode = sum % 7
     return a[wochentagCode]
 
@@ -25,13 +32,11 @@ def getJahresCode(jahr):
     if jahr >= 1900 and jahr <= 1999:
         y += 1
 
-    return y
+    return int(y)
 
 def getMonatsCode(monat, jahr):
     a = [6, 2, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4]
     monatsCode = a[monat - 1]
-    if monat <= 2:
-        monatsCode -= 1
     return monatsCode
 
 def isSchaltjahr(jahr):
